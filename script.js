29f934acedf5047d0a6a874d86bb6fcbfad986da
raphael <raphael.zwick8@gmail.com>
2025-05-06 15:52:09 +0200

    add fetchAPI()

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const BASE_URL ="https://pokeapi.co/api/v2/"
+// Ausschließlich Pokemon der ersten Generation werden abgefragt 
+const BASE_URL ="https://pokeapi.co/api/v2/pokemon?limit=150&offset=0"
 
 function init(){
     console.log("connect");
+    fetchAPI();
 }
+
+async function fetchAPI(){
+    try {
+        let response = await fetch(BASE_URL);
+        if(response.ok){
+            let responseAsJson = await response.json();
+            console.log(responseAsJson);
+        }else{
+            const errorInfo = await response.text();
+            console.log("Fehlermeldung des Servers: ", errorInfo);
+        }
+    } catch (error) {
+        console.error("Fehler beim der API abfrage -> BASE_URL " )
+        
+        
+    }
+}
+
